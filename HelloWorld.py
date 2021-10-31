4ca7c362bebfedd6ef4ceb2abc5eda56ac940d73
JitendraMishra16 <93475135+JitendraMishra16@users.noreply.github.com>
2021-10-31 18:26:22 +0530

    Added some comments

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,5 +1,7 @@
+# This is a program for Hello world...
 print ("Helow World!")
 print ("Helow Asia!")
 print ("Helow India!")
 print ("Helow West-Bengal!")
 print ("Helow Burdwan!")
+# End of program
